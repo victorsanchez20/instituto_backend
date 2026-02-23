a68fe4dab00bd7a5c529384ee27c66879941f78f
Victor <sanchezcueva20va@gmail.com>
2026-02-23 02:27:59 -0500

    Add url of frontend in CORS

--- src/main/java/com/instituto_api/config/WebConfig.java
+++ src/main/java/com/instituto_api/config/WebConfig.java
@@ -11,7 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins("http://localhost:4200",
+                            "https://victorsanchez20.github.io"
+            )
             .allowedMethods("GET","POST","DELETE","PUT","OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
